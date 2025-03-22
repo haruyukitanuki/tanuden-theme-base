8f0d8043566d420313296e4045669b07bbab1d64
Haruyuki Tanukiji <haruyuki@tanu.ch>
2025-03-23 01:37:25 +0800

    Clean up and update metadata

--- src/contexts.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import { GameDataState } from "./types/game-data-state";
-
-export const GameDataContext = createContext<{
-  gameDataState?: GameDataState;
-  setGameDataState: (gameDataState: GameDataState) => void;
-} | null>(null);
