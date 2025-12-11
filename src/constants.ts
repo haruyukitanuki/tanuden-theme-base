03867a6c8b3b561ed0d51e363cbd7f8091a3d925
Haruyuki Tanukiji <haruyuki@tanu.ch>
2025-12-11 23:16:05 +0800

    Update deps and implemented dynamic port usage

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,10 @@
 // ここで定義した定数は、アプリケーション全体で利用できます。
 
 // 下の値は、触らないでください。
-export const ENGINE_MODE = "local";
-export const ENGINE_SOCKET_HOST = `http://${window.location.hostname}:58680`;
+const currentURL = new URL(window.location.href);
+
+export const ENGINE_SOCKET_HOST =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_APP_ENGINE_SOCKET_HOST ?? "http://localhost:58680"
+    : `${currentURL.protocol}//${currentURL.host}`;
 export const PACKAGE_NAME = __THEME_NAME__;
-export const BASE_URL = import.meta.env.BASE_URL;
